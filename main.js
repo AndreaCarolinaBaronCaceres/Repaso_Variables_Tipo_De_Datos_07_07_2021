d16acd41804077d9e55018e0e8c500fc7c8090a4
AndreaCarolinaBaronCaceres <acbaca6789@gmail.com>
2021-07-07 21:08:20 -0500

    Repaso de las Variables 07 07 2021

---
+++ main.js
@@ -0,0 +1,10 @@
+
+nombreCompleto = `Andrea Carolina Baron :v`;
+miEdad = 21;
+soyUnaAlumna = true;
+ciudadResidencia = `FloridaBlanca` ;
+
+console.log(nombreCompleto, typeof(nombreCompleto)) ;
+console.log(miEdad, typeof(miEdad)) ;
+console.log(soyUnaAlumna, typeof(soyUnaAlumna));
+
